8189b62656d12bdc06c15106b6733687aeae4e29
Mira Chen <mirasc25@gmail.com>
2015-02-17 10:41:52 -0600

    setup

---
+++ Bo/Bo/FacebookBridgingHeader.h
@@ -0,0 +1,15 @@
+//
+//  FacebookBridgingHeader.h
+//  OddOneOut
+//
+//  Created by Mira Chen on 1/29/15.
+//  Copyright (c) 2015 OddOneOut. All rights reserved.
+//
+
+#ifndef OddOneOut_FacebookBridgingHeader_h
+#define OddOneOut_FacebookBridgingHeader_h
+
+#endif
+
+
+#import <FacebookSDK/FacebookSDK.h>
