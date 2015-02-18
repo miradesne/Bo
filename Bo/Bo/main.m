4c6d664f959502a98a45b36a7095967aa17439ed
Mira Chen <mirasc25@gmail.com>
2015-02-18 13:34:04 -0600

    back to objective-c

---
+++ Bo/Bo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Bo
+//
+//  Created by Mira Chen on 2/18/15.
+//  Copyright (c) 2015 MiraStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
